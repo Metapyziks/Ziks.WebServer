3bcef6c182601f3c6ea7f556b3d1091f62e5d0c3
James King <james.king@facepunchstudios.com>
2017-01-05 23:08:51 +0000

    Update Program.cs

--- Examples/SimpleExample/Program.cs
+++ Examples/SimpleExample/Program.cs
@@ -5,39 +5,21 @@ using Ziks.WebServer;
 
 namespace SimpleExample
 {
-    public interface IProgram
-    {
-        void WriteLine( string message );
-    }
-
     public class Program : IProgram
     {
         [STAThread]
         static void Main( string[] args )
         {
-            var program = new Program();
-            program.Run();
-        }
-
-        public void Run()
-        {
             var server = new Server();
 
             server.AddPrefix( "http://+:8080/" );
-
-            server.Components.Add<IProgram>( this );
             server.AddControllers( Assembly.GetExecutingAssembly() );
 
-            Task.Run( () => server.Run() );
+            Task.Run( server.Run );
 
             Console.ReadKey( true );
 
             server.Stop();
-        }
-
-        public void WriteLine( string message )
-        {
-            Console.WriteLine( message );
         }
     }
 }
