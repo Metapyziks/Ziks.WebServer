a45e25d9599e910201fed5c7e335ba6d4f71a54c
James King <james.king@facepunchstudios.com>
2017-01-06 16:28:24 +0000

    Updated nuget packaging script

--- nuget-pack.bat
+++ nuget-pack.bat
@@ -1,7 +1,14 @@
 @echo off
 
+set RELEASENOTES="None specified."
+echo %RELEASENOTES%
+
+if not %1=="" set RELEASENOTES=%1
+echo %RELEASENOTES%
+
+
 pushd %~dp0\Ziks.WebServer
 
-nuget pack Ziks.WebServer.csproj -OutputDirectory "..\packages" -Properties Configuration=Release
+nuget pack Ziks.WebServer.csproj -OutputDirectory "..\packages" -Properties Configuration=Release;releaseNotes=%RELEASENOTES%
 
 popd
